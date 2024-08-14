684739b4b2e797487a308273ceeb62c20a570b01
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2024-08-14 22:58:19 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arpithasudhir.github.io',
-  base: 'arpithasudhir.github.io'
+  
   
   
 });
